0b6e467d20e9198d294f787494d84ba2b31d0615
Oscar Cowdery Lack <oscar@lightningvisuals.com>
2022-04-26 15:18:40 +1000

    fix: Link against ffmpeg dependencies correctly
    FFmpeg has various dependencies that we need to link against.
    pkg_check_modules stores these in ${_component}_LIBRARIES.
    
    If pkg_check_modules is successful, then the subsequent
    find_path/find_libraries calls are redundant, since the relevant
    variables are already set by pkg_check_modules.
    
    If pkg_check_modules isn't successful or we don't have pkg-config, then
    it isn't very likely that find_path/find_libraries will do anything
    useful, since even if they happen to find the ffmpeg libraries
    themselves, they have no way of knowing about their dependencies.
    
    Therefore, let's simplify this logic by only using pkg_check_modules to
    find the ffmpeg libraries/include dirs.

--- src/CMakeModules/FindFFmpeg.cmake
+++ src/CMakeModules/FindFFmpeg.cmake
@@ -57,43 +57,9 @@ endmacro()
 # include directories.
 #
 macro(find_component _component _pkgconfig _library _header)
-
-  if (NOT WIN32)
-     # use pkg-config to get the directories and then use these values
-     # in the FIND_PATH() and FIND_LIBRARY() calls
-     find_package(PkgConfig)
-     if (PKG_CONFIG_FOUND)
-       pkg_check_modules(${_component} ${_pkgconfig})
-     endif ()
-  endif (NOT WIN32)
-
-  find_path(${_component}_INCLUDE_DIRS ${_header}
-    HINTS
-      ${PC_LIB${_component}_INCLUDEDIR}
-      ${PC_LIB${_component}_INCLUDE_DIRS}
-    PATH_SUFFIXES
-      ffmpeg
-  )
-
-  find_library(${_component}_LIBRARIES NAMES ${_library}
-      HINTS
-      ${PC_LIB${_component}_LIBDIR}
-      ${PC_LIB${_component}_LIBRARY_DIRS}
-  )
-
-  set(${_component}_DEFINITIONS  ${PC_${_component}_CFLAGS_OTHER} CACHE STRING "The ${_component} CFLAGS.")
-  set(${_component}_VERSION      ${PC_${_component}_VERSION}      CACHE STRING "The ${_component} version number.")
-
+  find_package(PkgConfig)
+  pkg_check_modules(${_component} ${_pkgconfig})
   set_component_found(${_component})
-
-  mark_as_advanced(
-    ${_component}_LIBRARY_DIRS
-    ${_component}_INCLUDE_DIRS
-    ${_component}_LIBRARIES
-    ${_component}_DEFINITIONS
-    ${_component}_VERSION
-  )
-
 endmacro()
 
 
